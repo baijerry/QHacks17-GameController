70c5942bb3701ae51da54999c4a44bbd9724b1c3
Jerry Bai <jerrybai1@hotmail.com>
2017-02-05 07:17:03 -0500

    stuff

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <Servo.h>
+//#include <SoftwareSerial.h>
 
 //-------------------------------
 // VARIABLES
@@ -7,7 +8,7 @@
 
 enum MODE { M_IDLE = 0, M_GAME, M_FINISH };
 MODE mode;
-
+//SoftwareSerial serial_servo(13, 12); //RX, TX; only 12 is used
 bool isHard;
 
 /*
@@ -47,11 +48,20 @@ int CheckEmptyCups();
 void clearArray();
 int genRandCup();
 int DetectCupChange(int win_cup); //Polls cups, detects removed cups, sends data, updates pts array
+void debug();
+void setLED(int win_cup);
+void flashfront();
+void setLEDs_off();
+void setLEDs_on();
+//void printcupstatus();
 
 //-------------------------------
 // SETUP
 //-------------------------------
 void setup() {
+  Serial.begin(9600);
+  //serial_servo.begin(9600);
+  
   pinMode (pin_cup1, INPUT);
   pinMode (pin_cup2, INPUT);
   pinMode (pin_cup3, INPUT);
@@ -74,6 +84,9 @@ void setup() {
 
   mode = M_IDLE;
   isHard = false;
+
+  flashfront();
+  delay(500);
 }
 
 //-------------------------------
@@ -81,27 +94,29 @@ void setup() {
 //-------------------------------
 void loop(){
   if (mode == M_IDLE){
-    //TODO: wait for starting game command (serial input) (servo always on)
-    while(0){
+    Serial.println("Enter Letter to Start Game");
+    while (!Serial.available()){
       delay(100);
     }
-
+    
     if (CheckEmptyCups() != 0){
       //not ready to start
-
-      //TODO: print out not ready on serial (not a node message)
+      Serial.println("Not all cups are in");
     }
     while (CheckEmptyCups()!=0){
-      delay(100);
+      //printcupstatus();
+      delay(1000);
     }
-    //TODO: send ready command (NODE:ready:1)
+    Serial.println("Starting Game!");
+    
+    //Send signal (ready)
+    Serial.print("NODE:ready:1");
+    
+    Serial.println("");
 
-    if (isHard){
-      digitalWrite(pin_servo, HIGH);
-    }
-    else {
-      digitalWrite(pin_servo, LOW);
-    }
+    //start servo
+    //digitalWrite(pin_servo, HIGH);
+    //serial_servo.write('i');
 
     mode = M_GAME;
   }
@@ -109,7 +124,7 @@ void loop(){
   if (mode == M_GAME){
     //start game
 
-    //clear array (all cups in)
+    //clear pts array (all cups in)
     clearArray();
 
     //generate random points cup (-1 if game over)
@@ -121,19 +136,23 @@ void loop(){
       if (DetectCupChange(win_cup)){ //Polls cups, detects removed cups, sends data, updates pts array
         //cup was pulled
         win_cup = genRandCup();
+        setLED(win_cup);
       }
       delay(10);
     }
 
     //all 6 cups removed
 
-    //TODO: Send final game data from pts_array
+    //send signal (end)
+    Serial.print("NODE:end:1");
+    Serial.println("");
 
     mode == M_FINISH;
   }
 
   if (mode == M_FINISH){
-    digitalWrite(pin_servo, LOW);
+    //digitalWrite(pin_servo, LOW);
+    //serial_servo.write('o');
     mode == M_IDLE;
   }
 }
@@ -141,6 +160,97 @@ void loop(){
 //-------------------------------
 // HELPER FUNCTIONS
 //-------------------------------
+void print_pts_array(){
+  for (int i = 0; i < 6; i++)
+    Serial.println(pts_array[i]);
+}
+
+void printcupstatus(){
+  //poll all pins
+  if (digitalRead(pin_cup1) == HIGH){
+    Serial.print("Cup 1: In");
+  }else {
+    Serial.print("Cup 1: Out");
+  }
+    
+  if (digitalRead(pin_cup2) == HIGH){
+    Serial.print("Cup 2: In");
+  }else {
+    Serial.print("Cup 2: Out");
+  }
+    
+  if (digitalRead(pin_cup3) == HIGH){
+    Serial.print("Cup 3: In");
+  }else {
+    Serial.print("Cup 3: Out");
+  }
+    
+  if (digitalRead(pin_cup4) == HIGH){
+    Serial.print("Cup 4: In");
+  }else {
+    Serial.print("Cup 4: Out");
+  }
+    
+  if (digitalRead(pin_cup5) == HIGH){
+    Serial.print("Cup 5: In");
+  }else {
+    Serial.print("Cup 5: Out");
+  }
+    
+  if (digitalRead(pin_cup6) == HIGH){
+    Serial.print("Cup 6: In");
+  }else {
+    Serial.print("Cup 6: Out");
+    }
+}
+
+void flashfront(){
+  Serial.println("flashing front");
+  for (int i =0; i < 6; i++){  
+    setLEDs_off();
+    delay(200);
+    setLEDs_on();
+    delay(200);
+  }
+    setLEDs_off();
+}
+
+void debug(){
+  Serial.println("Debug:");
+  while(!Serial.available()){
+    delay(10);
+  }
+  
+  int todo = Serial.read();
+
+  switch (todo){
+    case 0:
+      print_pts_array();
+      break;
+    case 1:
+
+      break;
+  }
+}
+
+void setLEDs_off(){
+  digitalWrite(pin_led1, LOW);
+  digitalWrite(pin_led2, LOW);
+  digitalWrite(pin_led3, LOW);
+  digitalWrite(pin_led4, LOW);
+  digitalWrite(pin_led5, LOW);
+  digitalWrite(pin_led6, LOW);
+}
+
+void setLEDs_on(){
+  digitalWrite(pin_led1, HIGH);
+  digitalWrite(pin_led2, HIGH);
+  digitalWrite(pin_led3, HIGH);
+  digitalWrite(pin_led4, HIGH);
+  digitalWrite(pin_led5, HIGH);
+  digitalWrite(pin_led6, HIGH);
+}
+
 
 //-------------------------------
 // MAIN FUNCTIONS
@@ -192,6 +302,11 @@ int genRandCup(){
   int randIndex = random(0, a_index);
   //Convert that into a cup number to return
   return existing_cup_array[randIndex] + 1;
+}
+
+void setLED(int win_cup){
+  setLEDs_off();
+  
 }
 
 //Polls cups, detects removed cups, sends data, updates pts array
@@ -247,11 +362,21 @@ int DetectCupChange(int win_cup){
         //cup gone
         flag_CupsChanged = true;
         if (i+1 == win_cup){
-          //TODO send cup is 10 data
+           
+          //send signal (shot, 10pts)
+          String cup = String(i+1);
+          String out = "NODE:shot:10," + cup;
+          Serial.print(out);
+          Serial.println("");
+          
           pts_array[i] = 10;
         }
         else{
-          //TODO send cup is 5 data
+          //send signal (shot, 5pts)
+          String cup = String(i+1);
+          String out = "NODE:shot:5," + cup;
+          Serial.print(out);
+          Serial.println("");
           pts_array[i] = 5;
         }
       }
--- servo_support/servo_support.ino
+++ servo_support/servo_support.ino
@@ -1,17 +1,53 @@
 #include <Servo.h>
+#include <SoftwareSerial.h>
 
 Servo servo;
 int pin_read = 12;
 
+SoftwareSerial serial_servo(8, 9); //RX, TX; only 8 is used
+
 int pos = 0;    // variable to store the servo position
+int move_flag;
 
 void setup() {
   servo.attach(2);
   servo.write(0);
+  serial_servo.begin(9600);
+  move_flag = false;
   pinMode(pin_read, INPUT);
 }
 
+
+
 void loop(){
+
+  if (serial_servo.available()){
+    char letter = serial_servo.read();
+    if (letter == 'o') {
+                move_flag = false;
+            }
+    if (letter == 'i') {
+                move_flag = true;
+    }
+  }
+
+  if (move_flag){
+    for (pos = 0; pos <= 180; pos += 2) {
+        servo.write(pos);
+        delay(35);
+      }
+      delay(500);
+      for (pos = 180; pos >= 0; pos -= 2) {
+        servo.write(pos);
+        delay(35);
+      }
+      delay(500);
+  }
+  else {
+    //nothing
+  }
+
+/*  
   if (digitalRead(pin_read) == HIGH){
     delay(300);
     if (digitalRead(pin_read) == HIGH){
@@ -27,6 +63,6 @@ void loop(){
       delay(500);
     }
   }
+*/
 
-  delay(10);
 }
